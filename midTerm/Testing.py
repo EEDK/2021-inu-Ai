0ed43f40e226e7801111438b8cc8829777f7ab2a
EEDK <kde9604@naver.com>
2021-04-17 20:55:58 +0900

    Add numpy

--- midTerm/Testing.py
+++ midTerm/Testing.py
@@ -1,39 +1,51 @@
-from math import exp
+import math
+from random import seed
 
+'''
+Relu 사용을 위한 내용
 
-# Calculate neuron activation for an input
+import numpy as np
+
+def relu(x):
+    return np.maximum(0, x)
+
+'''
+
+# weight summary 과정
 def activate(weights, inputs):
     activation = weights[-1]
+
     for i in range(len(weights) - 1):
         activation += weights[i] * inputs[i]
     return activation
 
 
-# Transfer neuron activation
-def transfer(activation):
-    return 1.0 / (1.0 + exp(-activation))
+# 순방향 전파, 시그모이드 = s(x) = e(x) / 1 + e(x)
+def transferNomal(x):
+    return math.exp(x) / (1.0 + math.exp(x))
 
-
-# Forward propagate input to a network output
-def forward_propagate(network, row):
-    inputs = row
+# 순방향 전파 함수
+def forwardPropagate(network, inputs):
     for layer in network:
-        new_inputs = []
-        for neuron in layer:
-            activation = activate(neuron['weight'], inputs)
-            neuron['output'] = transfer(activation)
-            new_inputs.append(neuron['output'])
-        inputs = new_inputs
+        updateInput = []
+        for net in layer:
+            activation = activate(net['weight'], inputs)
+            net['output'] = transferNomal(activation)
+            updateInput.append(net['output'])
+        inputs = updateInput
     return inputs
 
 
+
 # Make a prediction with a network
-def predict(network, row):
-    outputs = forward_propagate(network, row)
+def predict(network, expeceted):
+    outputs = forwardPropagate(network, expeceted)
     return outputs.index(max(outputs))
 
 
 # Test making predictions with the network
+
+seed(1)
 dataset = [
     [3.5064385449265267, 2.34547092892632525, 0],
     [4.384621956392097, 3.4530853889904205, 0],
@@ -46,8 +58,16 @@ dataset = [
     [5.032436157202415, 8.650625621472184, 1],
     [4.095084253434162, 7.69104329159447, 1]
 ]
-
-network = [[{'weight': [-0.6860256251179954, 1.6391986279465303, 0.8656386006221861], 'output': 0.9999766221827988, 'delta': -6.399043714642953e-06}, {'weight': [-0.5847679382509199, 0.5653278882929079, 0.22257578341987955], 'output': 0.8481933965606284, 'delta': 0.01183959285345359}], [{'weight': [0.9470961864963364, -0.07314796910860888, -0.9018956759236904], 'output': 0.5413806520720025, 'delta': -0.13441812532928651}, {'weight': [-1.13210406807162, 0.8265002311167667, 0.7313031253516632], 'output': 0.5357529036080855, 'delta': 0.11546834090803118}]]
+network = [
+    [
+        {'weight': [-0.6860256251179954, 1.6391986279465303, 0.8656386006221861], 'output': 0.9999766221827988, 'delta': -6.399043714642953e-06}
+        , {'weight': [-0.5847679382509199, 0.5653278882929079, 0.22257578341987955], 'output': 0.8481933965606284, 'delta': 0.01183959285345359}
+    ],
+    [
+        {'weight': [0.9470961864963364, -0.07314796910860888, -0.9018956759236904], 'output': 0.5413806520720025, 'delta': -0.13441812532928651},
+        {'weight': [-1.13210406807162, 0.8265002311167667, 0.7313031253516632], 'output': 0.5357529036080855, 'delta': 0.11546834090803118}
+    ]
+]
 
 for row in dataset:
     prediction = predict(network, row)
